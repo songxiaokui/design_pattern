4c892aea255e84ab15d8fedb0476b5a72638e7ab
宋晓奎 <austsxk@163.com>
2021-01-09 18:45:53 +0800

    # first init project && factory mode

---
+++ Factory/factory_test.go
@@ -0,0 +1,25 @@
+package Factory
+
+import "testing"
+
+/*
+@Time    : 2021/1/9 13:30
+@Author  : austsxk
+@Email   : austsxk@163.com
+@File    : factory_test.go
+@Software: GoLand
+*/
+
+func TestNewCars(t *testing.T) {
+	brands1 := NewCars("WLHG").GetCarBrandName()
+	if brands1 != "五菱宏光" {
+		t.Fatalf("v 的值错误，期望值：五菱宏光")
+	}
+	brands2 := NewCars("SHDZ").GetCarBrandName()
+	if brands2 != "上海大众" {
+		t.Fatalf("v 的值错误，期望值：上海大众")
+	}
+
+}
+
+//  go test factory_test.go factory.go
